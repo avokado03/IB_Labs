b1c6f5d2a12eb5a4bcfa1f5c7309faf250daf8e3
Балашова Ангелина <angelina.balashova@simbirsoft.com>
2022-04-06 02:00:21 +0300

    Добавьте файлы проекта.

---
+++ IB_Labs/Main.Designer.cs
@@ -0,0 +1,48 @@
+﻿
+namespace IB_Labs
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(344, 272);
+            this.Name = "Main";
+            this.Text = "Главная";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
