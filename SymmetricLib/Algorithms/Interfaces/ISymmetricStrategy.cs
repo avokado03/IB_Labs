d1acca74edfff4da253b8d19138fb2f9ba06edc5
Балашова Ангелина <angelina.balashova@simbirsoft.com>
2022-04-06 20:31:44 +0300

    Контракты

---
+++ SymmetricLib/Algorithms/Interfaces/ISymmetricStrategy.cs
@@ -0,0 +1,26 @@
+﻿using System.Security.Cryptography;
+
+namespace SymmetricLib.Algorithms.Interfaces
+{
+    /// <summary>
+    /// Контракт для стратегий алгоритмов
+    /// </summary>
+    interface ISymmetricStrategy
+    {
+        /// <summary>
+        /// Метод шифрования
+        /// </summary>
+        /// <param name="path">Путь к файлу</param>
+        /// <param name="password">Пароль</param>
+        /// <param name="cipherMode">Режим работы алгоритма</param>
+        void Encrypt(string path, string password, CipherMode cipherMode);
+
+        /// <summary>
+        /// Метод дешифрации
+        /// </summary>
+        /// <param name="path">Путь к зашифрованному файлу</param>
+        /// <param name="password">Пароль</param> 
+        /// <param name="cipherMode">Режим работы алгоритма</param>
+        void Decrypt(string path, string password, CipherMode cipherMode);
+    }
+}
---
+++ SymmetricLib/Models/ResultModel.cs
@@ -0,0 +1,36 @@
+﻿namespace SymmetricLib.Models
+{
+    /// <summary>
+    /// Модель результата обработки
+    /// файла алгоритмом шифрования
+    /// </summary>
+    public class AlgorithmResultModel
+    {
+        /// <summary>
+        /// Наименование алгоритма
+        /// </summary>
+        public string AlgorithmName { get; set; }
+
+        /// <summary>
+        /// Режим работы алгоритма
+        /// </summary>
+        public string AlgorithmMode { get; set; }
+
+        /// <summary>
+        /// Время, затраченное на шифрацию, с
+        /// </summary>
+        public double EncryptionTime { get; set; }
+
+        /// <summary>
+        /// Время, затраченное на дешифрацию, с
+        /// </summary>
+        public double DecryptionTime { get; set; }
+
+        /// <summary>
+        /// Общее время, затраченное на обработку файла
+        /// </summary>
+        public double GeneralTime { 
+            get { return EncryptionTime + DecryptionTime; } 
+        }
+    }
+}
